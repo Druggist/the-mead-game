24d95ab62e12d1a071d49fdc8428f0fea0375f10
Druggist <m.najdora@gmail.com>
2018-07-25 20:29:32 +0200

    Front page styling

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./Card.css";
 
 class Card extends React.Component {
 	render() {
 	    let title = this.props.title ? <h4>{this.props.title}</h4> : "";
 		let image = this.props.image ? <img src={this.props.image} alt={title}/> : null;
 		return (
-			<Link to={this.props.to}>
+			<Link to={this.props.to} className="card">
 				{image}
 				{title}
 			</Link>
--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,17 +1,38 @@
 import React from "react";
-import Background from "../Components/Background/Background";
+import { Grid, Row, Col } from "react-flexbox-grid";
+import Bubbles from "../Components/Bubbles/Bubbles";
+import Waves from "../Components/Waves/Waves";
 import Card from "../Components/Card/Card";
+import PoserImg from "../Components/Card/images/poser.jpg";
+import WarriorImg from "../Components/Card/images/warrior.jpg";
 
 class Menu extends React.Component {
 	render() {
 		return (
-			<Background>
-                <h1>THE MEAD SONG GAME</h1>
-                <h2>Choose your fate:</h2>
-                <Card to="/" title="Poser & wimp"/>
-                <Card to="/" title="True metal warrior"/>
-                <Card to="/" title="Rules"/>
-			</Background>
+			<div>
+				<Bubbles/>
+				<Grid fluid>
+					<Row center="xs">
+						<Col xs>
+							<h1>THE MEAD SONG GAME</h1>
+						</Col>
+					</Row>
+					<Row center="xs">
+						<Col xs>
+							<h2>CHOOSE YOUR FATE</h2>
+						</Col>
+					</Row>
+					<Row around="xs" center="xs">
+						<Col xs={6} >
+							<Card to="/" title="Poser & wimp" image={PoserImg} style={{maxWidth: "400px!important"}}/>
+						</Col>
+						<Col xs={6}>
+							<Card to="/" title="True metal warrior" image={WarriorImg}/>
+						</Col>
+					</Row>
+				</Grid>
+				<Waves/>
+			</div>
 		);
 	}
 }
