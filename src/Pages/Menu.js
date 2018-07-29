7f2b900d506813763d8cc51c96215bde9d387309
Druggist <m.najdora@gmail.com>
2018-07-29 19:20:54 +0200

    Info & support section

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -4,35 +4,70 @@ import Waves from "../Components/Waves/Waves";
 import Card from "../Components/Card/Card";
 import PoserImg from "../Components/Card/images/poser.jpg";
 import WarriorImg from "../Components/Card/images/warrior.jpg";
+import BomImg from "../Components/Card/images/bom.jpg";
 
 class Menu extends React.Component {
 	render() {
 		return (
-			<Grid fluid>
-				<Row center="xs">
-					<Col xs>
-						<h1>THE MEAD SONG GAME</h1>
-					</Col>
-				</Row>
-				<Row center="xs">
-					<Col xs>
-						<h2>CHOOSE YOUR FATE</h2>
-					</Col>
-				</Row>
-				<Row around="xs" center="xs">
-					<Col xs={6} >
-						<Card to="/play/poser" title="Wimp & poser" image={PoserImg}/>
-					</Col>
-					<Col xs={6}>
-						<Card to="/play/warrior" title="True metal warrior" image={WarriorImg}/>
-					</Col>
-				</Row>
-				<Row bottom="xs">
-					<Col xs={12}>
-						<Waves/>
-					</Col>
-				</Row>
-			</Grid>
+			<div className="App">
+				<Grid fluid>
+					<Row center="xs">
+						<Col xs>
+							<h1>THE MEAD SONG GAME</h1>
+						</Col>
+					</Row>
+					<Row center="xs">
+						<Col xs>
+							<h2>CHOOSE YOUR FATE</h2>
+						</Col>
+					</Row>
+					<Row around="xs" center="xs">
+						<Col xs={6} >
+							<Card to="/play/poser" title="Wimp & poser" image={PoserImg}/>
+						</Col>
+						<Col xs={6}>
+							<Card to="/play/warrior" title="True metal warrior" image={WarriorImg}/>
+						</Col>
+					</Row>
+					<Row bottom="xs" style={{minHeight: "100px"}}></Row>
+				</Grid>
+				<div className="more-container">
+					<Waves/>
+					<div className="more">
+						<Grid fluid>
+							<Row center="xs" style={{flex: "0 1 auto"}}>
+								<Col xs={12}>
+									<h3 style={{animation: "attractor 3s linear infinite", fontSize: "24px", marginTop: "-15px"}}>Want to know more?</h3>
+								</Col>
+							</Row>
+							<Row center="xs">
+								<Col xs={6}>
+									<h3>Ancient inscription</h3>
+									<blockquote className="inscription">
+										<span>It's not an ordinary song,</span>
+										<span>It's a driiinking  gaaaame!</span>
+										<span>Like all great games it is really easy to learn,</span>
+										<span>When you hear this word: <strong>MEAD</strong>,</span>
+										<span>It's time to <strong>DRINK</strong>!</span>
+										<span>And when you hear this word: <strong>ANOTHER</strong>,</span>
+										<span>It's also time to <strong>DRINK</strong>!</span>
+										<cite>One true god</cite>
+									</blockquote>
+								</Col>
+								<Col xs={6}>
+									<h3>Become a patron</h3>
+									<a className="patron" target="_blank" href="http://fb.com/brothersofmetalofficial">
+										<div className="img-container">
+											<img src={BomImg} alt="support"/>
+										</div>
+										<h4>Support the band</h4>
+									</a>
+								</Col>
+							</Row>
+						</Grid>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
